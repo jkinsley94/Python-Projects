60a7c5220796f37d5afe8ff0260268d4def92716
jkinsley <jakproductions94@gmail.com>
2023-02-23 17:45:02 -0700

    Created the start of a DND name generator

---
+++ DND.py
@@ -0,0 +1,8 @@
+import random
+
+print(input("What is your gender?\n "))
+
+
+def male_name():
+    name=('Andrew', 'Alexander', 'Aaron', 'Alex', 'Aiden', 'Anthony', 'Alphonse', 'Allen', 'Bofur' 'Brent' 'Bifur' 'Bilbo' '')
+

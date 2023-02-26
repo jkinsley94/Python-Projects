50f1559320246ab3b116a38857700e70417d020a
jkinsley <jakproductions94@gmail.com>
2023-02-25 21:57:56 -0700

    Corrected spelling

--- DND.py
+++ DND.py
@@ -6,14 +6,10 @@ def hello():
     print("Hello " + str(name) + "!")
     return
 
-
-
 def choices():
     choice=(input("Would you like a random weapon? y/n: "))
     if choice == "y":
-        print(random.choice(weapons) + " of " + random.choice(conditions))
-        
-        
+        print(random.choice(weapons) + " of " + random.choice(conditions))        
     else:
         if choice == "n":
             print("Very well.")
@@ -22,23 +18,16 @@ def races():
     choice=input("Would you like a random race? y/n: ")
     if choice == "y":
         print(random.choice(race) + " is your character race.")
-
     else:
         if choice == "n":
             print("Very well.")    
             
-race=[
-    'Dwarf', 'Dragonborn', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Orc', 'Half-Orc', 'Human', 'Tiefling'
-]
+race=['Dwarf', 'Dragonborn', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Orc', 'Half-Orc', 'Human', 'Tiefling']
 
-weapons=[
-    'Bow', 'Axe', 'Greatsword', 'Javelin', 'Dagger', 'Scimitar', 'Rapier', 'Quarterstaff'
-]
+weapons=[ 'Bow', 'Axe', 'Greatsword', 'Javelin', 'Dagger', 'Scimitar', 'Rapier', 'Quarterstaff']
 
 
-conditions=[
-    'ice', 'earth', 'wind', 'fire',  'sickness', 'bleeding', 'blinding', 'fear', 'exhaustion', 'constipation', 'poison', 'madness', ''
-]
+conditions=['ice', 'earth', 'wind', 'fire',  'sickness', 'bleeding', 'blinding', 'fear', 'exhaustion', 'constipation', 'poison', 'madness', '']
 
 def stats():
     const=random.randint(5,20)
@@ -49,9 +38,6 @@ def stats():
     charisma=random.randint(5,20)
     print("here are your stats as follows:\n Constitution, Strength Dexterity Intelligence Wisdom Charisma")
     print(const, strength, dex, intel, wisd, charisma)
-
-
-
 
 hello()
 choices()

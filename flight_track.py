6c0b9ffba7234b9f006e8bbd0c6fade0953b5bfc
jkinsley <jakproductions94@gmail.com>
2023-04-05 16:34:31 -0600

    not sure where this is going to go but adding it anyway

---
+++ flight_track.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import pandas as pd
+
+lon_min,lat_min=-125.974,30.038
+lon_max,lat_max=-68.748,52.214
+
+user_name=''
+password=''
+url_data=https://opensky-network.org/api/states/all?lamin=30.038&lomin=-125.974&lamax=52.214&lomax=-68.748

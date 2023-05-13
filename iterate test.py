53906a0439a7bd341052a8426798c8fcb1b44041
Jason Kinsley <123510189+FufferPuffer94@users.noreply.github.com>
2023-05-13 11:36:37 -0600

    --

--- iterate test.py
+++ iterate test.py
@@ -1,9 +1,10 @@
 def iterate():
     num = 0
-    while num < 100:
+    while num < 100000:
         num += 1
-    if num == 100:
-        print("it's 100!")
-    
+    if num == 200:
+        print("200!") #need to figure out why it doesn't print 200 and skips to the next print function
+    if num == 100000:
+        print("finsihed!")
 
-    iterate()
+iterate()
